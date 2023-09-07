3a5c742bf2d737fcab0b8701fc964de954833f20
bagassambega <bagassambega@gmail.com>
2023-09-07 23:46:22 +0700

    latihan praktikum 2 (1)

---
+++ Praktikum/Pra-Praktikum/Praktikum 2/intersect.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "boolean.h"
+#include "liststatik.c"
+
+int main() {
+    ListStatik l1, l2, l;
+    readList(&l1);
+    readList(&l2);
+    CreateListStatik(&l);
+    sortList(&l1, true);
+    sortList(&l2, true);
+    int i, j;
+    for (i = IDX_MIN; i < listLength(l1); i++) {
+        for (j = IDX_MIN; j < listLength(l2); j++) {
+            if (ELMT(l1, i) == ELMT(l2, j)) {
+                insertLast(&l, ELMT(l1, i));
+            }
+        }
+    }
+    sortList(&l, true);
+    printf("%d\n", listLength(l));
+    printList(l);
+    printf("\n");
+    return 0;
+}
