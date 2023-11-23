6b60716d83840205c6df85e10122e3d3b61aa00a
Bagas <bagassambega@gmail.com>
2023-11-23 14:22:48 +0700

    pra-praktikum 12

---
+++ Alstrukdat/Praktikum 12/bintree.c
@@ -0,0 +1,109 @@
+#include "boolean.h"
+#include "bintree.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+BinTree NewTree (ElType root, BinTree left_tree, BinTree right_tree){
+    BinTree p;
+    p = newTreeNode(root);
+    if (p != NULL){
+        LEFT(p) = left_tree;
+        RIGHT(p) = right_tree;
+    }
+    return p;
+}
+
+void CreateTree (ElType root, BinTree left_tree, BinTree right_tree, BinTree *p){
+    *p = newTreeNode(root);
+    if (*p != NULL) {
+        LEFT(*p) = left_tree;
+        RIGHT(*p) = right_tree;
+    }
+}
+
+Address newTreeNode(ElType val) {
+    Address p = (Address) malloc(sizeof(TreeNode));
+    if (p != NULL) {
+        ROOT(p) = val;
+        LEFT(p) = NULL;
+        RIGHT(p) = NULL;
+    }
+    return p;
+}
+
+void deallocTreeNode (Address p) {
+    free(p);
+}
+
+boolean isTreeEmpty (BinTree p) {
+    return p == NULL;
+}
+
+boolean isOneElmt (BinTree p) {
+    return !isTreeEmpty(p) && isTreeEmpty(LEFT(p)) && isTreeEmpty(RIGHT(p));
+}
+
+boolean isUnerLeft (BinTree p) {
+    return !isTreeEmpty(p) && !isTreeEmpty(LEFT(p)) && isTreeEmpty(RIGHT(p));
+}
+
+boolean isUnerRight (BinTree p) {
+    return !isTreeEmpty(p) && isTreeEmpty(LEFT(p)) && !isTreeEmpty(RIGHT(p));
+}
+
+boolean isBinary (BinTree p) {
+    return !isTreeEmpty(p) && !isTreeEmpty(LEFT(p)) && !isTreeEmpty(RIGHT(p));
+}
+
+void printPreorder(BinTree p) {
+    if (isTreeEmpty(p)) {
+        printf("()");
+    }
+    else {
+        printf("(");
+        printf("%d", ROOT(p));
+        printPreorder(LEFT(p));
+        printPreorder(RIGHT(p));
+        printf(")");
+    }
+}
+
+void printInorder(BinTree p) {
+    if (isTreeEmpty(p)) {
+        printf("()");
+    }
+    else {
+        printf("(");
+        printInorder(LEFT(p));
+        printf("%d", ROOT(p));
+        printInorder(RIGHT(p));
+        printf(")");
+    }
+}
+
+void printPostorder(BinTree p) {
+    printf("(");
+    if (!isTreeEmpty(p)) {
+        printPostorder(LEFT(p));
+        printPostorder(RIGHT(p));
+        printf("%d", ROOT(p));
+    }
+    printf(")");
+}
+
+void printwithIndentation(BinTree p, int h, int count) {
+    int i;
+    if (!isTreeEmpty(p)) {
+        for (i = 0; i < h * count; i++) {
+            printf(" ");
+        }
+        printf("%d\n", ROOT(p));
+        printwithIndentation(LEFT(p), h, count + 1);
+        printwithIndentation(RIGHT(p), h, count + 1);
+    }
+}
+
+
+void printTree(BinTree p, int h) {
+    printwithIndentation(p, h, 0);
+}
